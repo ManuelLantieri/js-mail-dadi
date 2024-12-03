5957e8e1cd8f5d5367b8334ec5dc25da7e31c18f
manuel lantieri <manuel.lantieri02@gmail.com>
2024-12-03 18:33:55 +0100

    feat dadi: scelta utente

---
+++ dadi/script.js
@@ -0,0 +1,22 @@
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+// Stabilire il vincitore, in base a chi fa il punteggio più alto.
+// Prima di partire a scrivere codice poniamoci qualche domanda:
+// Che ci sia un array da qualche parte?
+// Se dobbiamo confrontare qualcosa che "cosa" ci serve?
+
+
+//1 lista con numeri da 1 a 6
+//chiedere all'utente di scegiere un numero da 1 a 6
+//fai in modo che il computer scelga un numero da 1 a sei
+//confrontando i due risultati determina il numero più alto
+//stampare il vincitore che ha scelto il numero più alto
+
+const numeriDado = [1, 2, 3, 4, 5, 6];
+
+let sceltaUtente;
+
+do {
+    sceltaUtente = parseFloat(prompt("scegli un numero da 1 a 6"))
+} while(parseFloat(sceltaUtente));
+alert(ciao)
+
